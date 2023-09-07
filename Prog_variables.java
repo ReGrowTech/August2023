45d6d3173f25bff44ae3fffda52fe2eb488e8dd1
fahad-akhtar <fahad201308@gmail.com>
2023-09-07 11:03:37 +0530

    JAVA prog using variables to store value and display them.

---
+++ Prog_variables.java
@@ -0,0 +1,17 @@
+package basic_prog;
+
+public class Prog_variables {
+	
+	public static void main(String args[])
+	{
+		int num=25;
+		String name = "Fahad Akhtar";
+		int year = 2023;
+		double marks=99.19;
+		System.out.println(" The Number is "+num+"\n Hi! My name is \""+name+"\"."+
+				"\n I started studying in ReGrow from \""+year+"\"."+
+				"\n My Marks Scored is "+marks);
+	}
+
+}
+
